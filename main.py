1a68d47344730a16c0e6b77f0efbd6ec14656543
Sleepy_Wolve <thewolve@duck.com>
2025-02-12 19:17:42 +0200

    add a pretty list for the letter count + settings

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import string
 
+#settings
+set_print_entire_book = False
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_words_count(text)
     letters_count = get_characters_count(text)
-    print(f"{text} \nword count: {word_count}\nLetters count: {letters_count}")
+    if set_print_entire_book:
+        print(text)
+    print(f"Word count: {word_count}\nLetters count\n{letters_count}")
     
 
 def get_words_count(book):
@@ -18,13 +24,22 @@ def get_book_text(path):
     
 def get_characters_count(text):
     letter_dict = {c: 0 for c in string.ascii_lowercase}
-    lowercase = text.lower()
     letter_dict[" "] = 0
+    lowercase = text.lower()
     for i in lowercase:
         if i in letter_dict:
             letter_dict[i] += 1
             
-    return letter_dict
+    return prettify(letter_dict)
+
+def prettify(dict):
+    del dict[" "]
+    pretty_string = ""
+    for letter in dict:
+        pretty_string += f"the '{letter}' has found {dict[letter]} times\n"
+
+    return pretty_string
+
 
 if __name__ == '__main__':
     main()
